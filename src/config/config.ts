2708c81ab2fe42d819feabde0296aba910344737
skeeDayo <steven_escopete@dlsu.edu.ph>
2023-12-12 15:15:21 +0800

    adjust styles

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,3 @@
-export const SERVER_URL = 'http://localhost:8080';
+export const SERVER_URL = 'http://192.168.1.13:8080';
 export const GET_ALL_COORDINATES_URI = '/get-all-coordinates';
 export const GET_MARKER_INFORMATION_URI = '/get-marker-info/'
